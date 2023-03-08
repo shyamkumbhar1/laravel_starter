519504e06a9097bcbe3d68ec2941fabd38d346cd
shyamkumbhar87 <111362532+shyamkumbhar87@users.noreply.github.com>
2023-03-08 18:07:55 +0530

    modify email files

--- app/Http/Controllers/Mail/mailController.php
+++ app/Http/Controllers/Mail/mailController.php
@@ -2,8 +2,14 @@
 
 namespace App\Http\Controllers\Mail;
 
+
+use App\Mail\SendTestMail;
+
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Mail;
+
+
 
 class mailController extends Controller
 {
@@ -15,6 +21,41 @@ class mailController extends Controller
             $msg->to("shyam@gmail.com","Advance Laravel project")
                 ->subject("Advance Laravel Series");
                
-        }
+        });    
+        echo "mail Send Sucessfully.";
+    }
+    function Mailable () {
+
+        mail::to("shyamkumbhar@gmail.com")->send(New SendTestMail);
+        echo "mail Send Sucessfully.";
+    }
+
+    function Markdown () {
+
+        // mail::to("shyamkumbhar@gmail.com")->send(New App\Mail\SendMarkDownMail);
+        // echo "mail Send Sucessfully.";
+    
+        $markdown = new \Illuminate\Mail\Markdown(view(), config('mail.markdown'));   
+        $data = ["Your data to be use in blade file"];
+        return $markdown->render("welcome", $data);
+    
+    
+    }
+    function previewTemplate () {
+        $markdown = new \Illuminate\Mail\Markdown(view(), config('mail.markdown'));   
+        $data = ["Your data to be use in blade file"];
+        $template =  $markdown->render("welcome", $data);
+    
+        $data = ["name"=> "shyam kumbhar"];
+        mail::send("email",$data,function($msg){
+            $msg->to("shyam@gmail.com","Advance Laravel project")
+                ->subject("Advance Laravel Series");
+               
+        });
+    
+        echo "email send Succesfully";
+        
+    
+    }
 }
-}
+
--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Mail\Markdown;
-use App\Http\Controllers\{ProductController,NormalHttpClient,GuzzleHttpClient,MutatorController};
-
 use Rap2hpoutre\LaravelLogViewer\LogViewerController;
+use App\Http\Controllers\{ProductController,NormalHttpClient,GuzzleHttpClient,MutatorController};
+use App\Http\Controllers\Mail\mailController;
+
 
 
 Route::get('/', function () {
@@ -21,53 +22,12 @@ Route::resource('products', ProductController::class);
 
 // Email Send using cron job 
 
-Route::get('/send-email', function () {
-    $data = ["name"=> "shyam kumbhar"];
-    // dd($name);
-
-    mail::send("email",$data,function($msg){
-        $msg->to("shyam@gmail.com","Advance Laravel project")
-            ->subject("Advance Laravel Series");
-           
-    });
-
-  
-
-    echo "mail Send Sucessfully.";
-});
-
-Route::get('/Mailable', function () {
-
-    mail::to("shyamkumbhar@gmail.com")->send(New App\Mail\SendTestMail);
-    echo "mail Send Sucessfully.";
-});
-Route::get('/mark-Down', function () {
-
-    // mail::to("shyamkumbhar@gmail.com")->send(New App\Mail\SendMarkDownMail);
-    // echo "mail Send Sucessfully.";
-
-    $markdown = new \Illuminate\Mail\Markdown(view(), config('mail.markdown'));   
-    $data = ["Your data to be use in blade file"];
-    return $markdown->render("welcome", $data);
+Route::get('/send-email',[mailController::class,'sendEmail'] );
+Route::get('/Mailable',[mailController::class,'Mailable'] );
+Route::get('/mark-Down',[mailController::class,'Markdown'] );
+Route::get('/preview-email-template',[mailController::class,'previewTemplate'] );
 
 
-});
-Route::get('/preview-email-template', function () {
-    $markdown = new \Illuminate\Mail\Markdown(view(), config('mail.markdown'));   
-    $data = ["Your data to be use in blade file"];
-    $template =  $markdown->render("welcome", $data);
-
-    $data = ["name"=> "shyam kumbhar"];
-    mail::send("email",$data,function($msg){
-        $msg->to("shyam@gmail.com","Advance Laravel project")
-            ->subject("Advance Laravel Series");
-           
-    });
-
-    echo "email send Succesfully";
-    
-
-});
 
 // Third Party Api Integration using Normal http client (CRUD)
 Route::get('/post',[NormalHttpClient::class,"all_data"]);
