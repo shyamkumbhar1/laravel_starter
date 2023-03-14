bc56d4bb46bff2d395a775e4e3bf896504aa2fc3
shyamkumbhar87 <111362532+shyamkumbhar87@users.noreply.github.com>
2023-03-14 21:34:28 +0530

    seeder Done

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 use Database\Seeders\UserSeeder;
 
@@ -15,16 +16,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        // Call Factory
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-
-        $this->call([
-            UserSeeder::class
-        ]);
+        User::factory()->count(10)->create();
+        // $this->call([
+        //     UserSeeder::class
+        // ]);
 
     }
 }
