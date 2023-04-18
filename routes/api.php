5ea897736e13ec1dcaa665de56b243633aee091d
shyamkumbhar1 <shyamkumbhar509@gmail.com>
2023-04-18 17:58:02 +0530

    clean code

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\Product1Controller;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,45 +17,3 @@ use App\Http\Controllers\Product1Controller;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-/* Laravel Sanctum start  */
-//ref : https://codelapan.com/post/laravel-8-rest-api-authentication-with-sanctum
-//API route for register new user 
-Route::post('/register', [App\Http\Controllers\API\AuthController::class, 'register']);
-//API route for login user
-Route::post('/login', [App\Http\Controllers\API\AuthController::class, 'login']);
-
-//Protecting Routes
-Route::group(['middleware' => ['auth:sanctum']], function () {
-    Route::get('/profile', function(Request $request) {
-        return auth()->user();
-    });
-
-    // API route for logout user
-    Route::post('/logout', [App\Http\Controllers\API\AuthController::class, 'logout']);
-
-/* Laravel Sanctum end  */
-
-});
-
-// Jwt Token Api start
-
-Route::group([
-    'middleware' => 'api',
-    'prefix' => 'auth'
-], function ($router) {
-    Route::post('/login', [AuthController::class, 'login']);
-    Route::post('/register', [AuthController::class, 'register']);
-    // Route::post('/logout', [AuthController::class, 'logout']);
-    // Route::post('/refresh', [AuthController::class, 'refresh']);
-    Route::get('/user-profile', [AuthController::class, 'userProfile']);   
-    // Route::get('/get-flightData', [AuthController::class, 'getFlightData']);    
-
-});
-
-// Jwt Token Api End
-
-
-// React Laravel Crud
-
-Route::resource('products',Product1Controller::class);
