c11db1cc0a6d9ccff1dfe59607b3715dd9a38374
Shyamkumbhar57 <shyam@datalogysoftware.com>
2023-08-07 13:22:18 +0530

    setup Done

---
+++ app/Models/User.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Country;
+use Laravel\Cashier\Billable;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable,Billable;
+
+
+    protected $fillable = [
+        'name',
+        'is_admin',
+        'professional_title',
+        'email',
+        'password',
+        'status'
+    ];
+    public function country()
+{
+    return $this->belongsTo(Country::class);
+}
+
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+}
