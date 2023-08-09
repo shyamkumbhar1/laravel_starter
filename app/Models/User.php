aaabdc3798f37f95fbd0e441c935c365036e3271
shyamkumbhar12 <shyam.kumbhar@invezzatechnologies.com>
2023-08-09 15:41:01 +0530

    add

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,14 +5,15 @@ namespace App\Models;
 use App\Models\Country;
 use Laravel\Cashier\Billable;
 use Laravel\Sanctum\HasApiTokens;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
+class User extends Authenticatable  implements JWTSubject
 {
-    use HasApiTokens, HasFactory, Notifiable,Billable;
+    use HasApiTokens, HasFactory, Notifiable, Billable;
 
 
     protected $fillable = [
@@ -24,9 +25,9 @@ class User extends Authenticatable
         'status'
     ];
     public function country()
-{
-    return $this->belongsTo(Country::class);
-}
+    {
+        return $this->belongsTo(Country::class);
+    }
 
 
     protected $hidden = [
@@ -38,4 +39,19 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\JWTAuthController;
 
 
@@ -10,11 +11,21 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('register', [JWTAuthController::class, 'register']);
-Route::post('login', [JWTAuthController::class, 'login']);
+// Route::post('register', [JWTAuthController::class, 'register']);
+// Route::post('login', [JWTAuthController::class, 'login']);
 
-Route::group(['middleware' => 'auth.jwt'], function () {
+// Route::group(['middleware' => 'auth.jwt'], function () {
 
-    Route::post('logout', [JWTAuthController::class, 'logout']);
+//     Route::post('logout', [JWTAuthController::class, 'logout']);
 
+// });
+
+//Jwt Auth
+Route::controller(AuthController::class)->group(function () {
+    Route::post('login', 'login');
+    Route::post('register', 'register');
+    Route::post('logout', 'logout');
+    Route::post('refresh', 'refresh');
 });
+
+
